1bb0862f1770f49dc2ff873215e333bd9cd81470
slasher314 <paulo.vb.souza@unesp.br>
2022-08-16 15:43:51 -0300

    pequena alteração na função divisor

--- divisor.js
+++ divisor.js
@@ -5,7 +5,7 @@ function divisor(arr)
     arr.forEach(element => {
         element>0?pos.push(element):neg.push(element);
     });
-    return([pos, neg]);
+    return[pos, neg];
 }
 
 console.log(divisor([1,2,-7,-22,0]))
